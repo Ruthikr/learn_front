213d91937dfaa73e5571bd857c8b97b31d6b1ad2
Ruthik <147206849+Ruthikr@users.noreply.github.com>
2024-11-07 18:16:22 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "http://localhost:8000/";
+const apiUrl = "https://aa51-2401-4900-62e0-6b3f-4c6a-368a-bf33-39e.ngrok-free.app/";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
